c04c2abfc909d90a0ba14487bfc501d441e68bfe
lance.boylan <eg01st3220@gmail.com>
2026-02-19 15:44:22 -0700

    Added the enums :)

---
+++ ConsoleAppEmpty/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEnums
+{
+    internal class Enums
+    {
+        
+    }
+
+    enum GameDifficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+
+    enum CharacterClass
+    {
+        Warrior,
+        Mage,
+        Rogue
+    }
+}
