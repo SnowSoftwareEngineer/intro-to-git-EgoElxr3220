11a0196914e85905e05a9dbe141cf310d0e8a550
Lance Boylan <eg01st3220@gmail.com>
2026-02-20 15:41:40 -0700

    Finished it :)

--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -10,18 +10,45 @@ public class Program
         {
             Console.WriteLine($"{(int)item}. {item}");
         }
-        int response = int.Parse(Console.ReadLine().ToLower().Trim());
+        difficulty = (GameDifficulty)Enum.Parse(typeof(GameDifficulty), Console.ReadLine().ToLower().Trim(), true);
+        string difficultyStr = "";
+
         CharacterClass characterClass = new CharacterClass();
         Console.WriteLine("Select a character class");
         foreach (var item in Enum.GetValues<CharacterClass>())
         {
             Console.WriteLine($"{(int)item}. {item}");
         }
-        int response2 = int.Parse(Console.ReadLine().ToLower().Trim());
+        characterClass = (CharacterClass)Enum.Parse(typeof(CharacterClass), Console.ReadLine().ToLower().Trim(), true);
+        string characterClassStr = "";
+
+        if (difficulty == GameDifficulty.Easy)
+        {
+            difficultyStr = difficulty.ToString();
+        }
+        else if (difficulty == GameDifficulty.Medium)
+        {
+            difficultyStr = difficulty.ToString();
+        }
+        else if (difficulty == GameDifficulty.Hard)
+        {
+            difficultyStr = difficulty.ToString();
+        }
+
+        if (characterClass == CharacterClass.Warrior)
+        {
+            characterClassStr = characterClass.ToString();
+        }
+        else if (characterClass == CharacterClass.Mage)
+        {
+            characterClassStr = characterClass.ToString();
+        }
+        else if (characterClass == CharacterClass.Rogue)
+        {
+            characterClassStr = characterClass.ToString();
+        }
 
 
-
-        Console.WriteLine( + "\n" + );
-        //Console.WriteLine($"You selected:\nDifficulty:");
+        Console.WriteLine($"You selected:\nDifficulty: {difficultyStr}\nCharacter Class: {characterClassStr}");
     }
 }
